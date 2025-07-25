773d921322b5ed8a67d860b0e21303bbfef86f1c
MBoussel <mb.boussel@gmail.com>
2025-07-25 12:08:38 +0200

    create card for project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router'
 import './App.css'
 import { useDarkMode } from "./contexts/ThemeContext";
 import Navbar from './components/Navbar';
+import 'devicon/devicon.min.css';
 
 function App() {
  const { theme } = useDarkMode();
